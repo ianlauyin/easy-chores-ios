162d764620b3f6a610378fbfd6c8b1891245d473
IanLau <ianlauyin@gmail.com>
2024-05-11 17:22:49 +0800

    Initial Commit

---
+++ easy-chores/easy_choresApp.swift
@@ -0,0 +1,17 @@
+//
+//  easy_choresApp.swift
+//  easy-chores
+//
+//  Created by Claire Chu on 11/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct easy_choresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
